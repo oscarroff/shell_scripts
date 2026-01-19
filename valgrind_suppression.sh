eb3913cd84a5a2cb57bcdad78fe71dbf6ee36b0b
Thomas Black-Roff <thblack-@c1r3p6.hive.fi>
2026-01-19 20:12:25 +0200

    feat: added gitignore to remove work that is not mine

--- valgrind_suppression.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-shopt -s nullglob
-args=()
-for supp in *.supp; do
-	args+=(--suppressions="$supp")
-done
-valgrind --leak-check=full --show-leak-kinds=all --track-fds=yes --track-origins=yes --trace-children=yes "${args[@]}" "$@"
