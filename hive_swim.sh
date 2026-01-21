02ad649d1f77a74b640fa89b54ffc5ff9897dc29
Thomas Black-Roff <thblack-@c1r3p6.hive.fi>
2026-01-21 09:38:33 +0200

    fix: allowed hive_swim to fetch without ssl certificate

--- hive_swim.sh
+++ hive_swim.sh
@@ -19,7 +19,7 @@ NC='\033[0m'
 
 # Fetch data
 feed_url="https://iot.fvh.fi/opendata/uiras/70B3D57050011458.geojson"
-feed_data=$(curl -s "$feed_url")
+feed_data=$(curl -sk "$feed_url")
 
 # Helper function for parsing data
 json_get() {
@@ -30,24 +30,24 @@ json_get() {
 temp_water=$(json_get "$feed_data" "['properties']['measurement']['temp_water']")
 
 # Print output
-echo "Ｏ＿ＲＯＦＦ  Ｈｉｖｅ  Ｓｗｉｍ  ？"
+echo "O _ R O F F   H i v e   S w i m ?"
 echo "Source: https://uiras.fvh.io/"
 echo
-if [ $(echo "$temp_water < 0" | bc) -eq 1 ]; then
+if [ $(echo "$temp_water < 0" | bc -l) -eq 1 ]; then
 	echo "Clearly not, even the thermometer has left the water."
-elif [ $(echo "$temp_water >= 0" | bc) -eq 1 ] \
-	&& [ $(echo "$temp_water < 1" | bc) -eq 1 ]; then
+elif [ $(echo "$temp_water >= 0" | bc -l) -eq 1 ] \
+	&& [ $(echo "$temp_water < 1" | bc -l) -eq 1 ]; then
 	echo "${BLUE}Watch out for the icebergs!"
-elif [ $(echo "$temp_water >= 1" | bc) -eq 1 ] \
-	&& [ $(echo "$temp_water < 15" | bc) -eq 1 ]; then
+elif [ $(echo "$temp_water >= 1" | bc -l) -eq 1 ] \
+	&& [ $(echo "$temp_water < 15" | bc -l) -eq 1 ]; then
 	echo "${CYAN}Brave souls only."
-elif [ $(echo "$temp_water >= 15" | bc) -eq 1 ] \
-	&& [ $(echo "$temp_water < 22" | bc) -eq 1 ]; then
+elif [ $(echo "$temp_water >= 15" | bc -l) -eq 1 ] \
+	&& [ $(echo "$temp_water < 22" | bc -l) -eq 1 ]; then
 	echo "${GREEN}What are you waiting for?"
-elif [ $(echo "$temp_water > 22" | bc) -eq 1 ] \
-	&& [ $(echo "$temp_water <= 30" | bc) -eq 1 ]; then
+elif [ $(echo "$temp_water > 22" | bc -l) -eq 1 ] \
+	&& [ $(echo "$temp_water <= 30" | bc -l) -eq 1 ]; then
 	echo "${YELLOW}Err what? Somebody left the hot tap on."
-elif [ $(echo "$temp_water > 30" | bc) -eq 1 ]; then
+elif [ $(echo "$temp_water > 30" | bc -l) -eq 1 ]; then
 	echo "${RED}If you're seeing this, we're doomed and the oceans have evaporated."
 fi
 echo "${NC}The water off mattolaituri is currently ${BOLD}${temp_water}°C${NC}"
